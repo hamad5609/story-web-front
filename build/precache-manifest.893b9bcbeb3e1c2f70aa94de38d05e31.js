3e9baf73fbc63878b88e8a1d165a34643cddfe56
hamad5609 <92583418+hamad5609@users.noreply.github.com>
2022-01-28 15:40:30 +0500

    Add files via upload

---
+++ build/precache-manifest.893b9bcbeb3e1c2f70aa94de38d05e31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "389ca883ecb3e0b41558161ad578ea35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ffcdde65bf9e8b06a41",
+    "url": "/static/js/2.bd9dd183.chunk.js"
+  },
+  {
+    "revision": "18e3ca66c69046fdfd0b",
+    "url": "/static/js/main.789e56d5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8db4e0571b82f1a6d01e85f226569b19",
+    "url": "/static/media/logo.8db4e057.png"
+  }
+]);
